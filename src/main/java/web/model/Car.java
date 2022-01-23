36d06054233674c4263ade10dfa182706fb4db83
max <sharg97@gmail.com>
2022-01-23 20:05:32 +0300

    1

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,37 @@
+package web.model;
+
+public class Car {
+    private String name;
+    private int model;
+    private int valueCar;
+
+    public Car(String name, int model, int value) {
+        this.name = name;
+        this.model = model;
+        this.valueCar = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getModel() {
+        return model;
+    }
+
+    public void setModel(int model) {
+        this.model = model;
+    }
+
+    public int getValueCar() {
+        return valueCar;
+    }
+
+    public void setValueCar(int valueCar) {
+        this.valueCar = valueCar;
+    }
+}
